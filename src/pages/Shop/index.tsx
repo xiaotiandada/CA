c545a4a936096fe66cf38677b6677eff577806cf
xiaotiandada <952822399@qq.com>
2020-10-21 20:58:28 +0800

    feat: list

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -7,7 +7,25 @@ import { DownloadOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/ic
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
-const list = [1, 2, 3, 4]
+const list = [
+    {
+        title: 'Default Card',
+        src: 'https://codepen.io/agathaco/embed/preview/zYBGXaz?height=265&theme-id=dark&default-tab=html,result',
+        iframe: `<iframe
+        height="265"
+        style="width: 100%"
+        scrolling="no"
+        title="Curious Bear"
+        src="https://codepen.io/agathaco/embed/preview/zYBGXaz?height=265&theme-id=dark&default-tab=html,result"
+        frameBorder="no"
+        loading="lazy"
+        // @ts-ignore
+        allowtransparency="true"
+        allowFullScreen={ true }>
+        See the Pen
+        <a href='https://codepen.io/agathaco/pen/zYBGXaz'>Curious Bear</a> by agathaco(<a href='https://codepen.io/agathaco'>@agathaco</a>) on <a href='https://codepen.io'>CodePen</a>.</iframe>`
+    }
+]
 
 const Shop = () => {
     return (
@@ -16,26 +34,13 @@ const Shop = () => {
             <StyledCardWrapper>
                 {
                     list.map((i, idx) => (
-                        <Card title="Default Card" style={{ overflow: 'auto' }} key={idx} extra={<a href="/">View</a>} actions={[
+                        <Card title={ i.title } style={{ overflow: 'auto' }} key={idx} extra={<a href={ i.src } target="_blannk" rel="noopener noreferrer">View</a>} actions={[
                             <DownloadOutlined key="download" />,
                             <EditOutlined key="edit" />,
                             <EllipsisOutlined key="ellipsis" />,
                         ]}>
                             {
-                                idx === 0 ?
-                                <iframe
-                                height="265"
-                                style={{ width: '100%' }}
-                                scrolling="no"
-                                title="Curious Bear"
-                                src="https://codepen.io/agathaco/embed/preview/zYBGXaz?height=265&theme-id=dark&default-tab=html,result"
-                                frameBorder="no"
-                                loading="lazy"
-                                // @ts-ignore
-                                allowtransparency="true"
-                                allowFullScreen={ true }>
-                                See the Pen
-                                <a href='https://codepen.io/agathaco/pen/zYBGXaz'>Curious Bear</a> by agathaco(<a href='https://codepen.io/agathaco'>@agathaco</a>) on <a href='https://codepen.io'>CodePen</a>.</iframe> : <div style={{ height: 265 }}></div>
+                                idx === 0 ? <div dangerouslySetInnerHTML={ { __html: i.iframe } }></div> : <div style={{ height: 265 }}></div>
                             }
                         </Card>
                     ))
